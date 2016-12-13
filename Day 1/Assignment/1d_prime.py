3c7ad32e8a66aa0c49019eebe9dc8e0216dc263a
Anas Khan <anas.khan96@outlook.com>
2016-12-14 03:13:43 +0530

    Update 1d_prime.py
    Replaced `n` with `i` on line 11 as the second iteration is supposed to go upto the square root of `i`.
    Added a pythonic way of approaching the same problem using list comprehension.

--- Day 1/Assignment/1d_prime.py
+++ Day 1/Assignment/1d_prime.py
@@ -8,7 +8,7 @@ l = []
 import math
 for i in range(n):
     flag = 0
-    for j in range(2, math.ceil(math.sqrt(n))):
+    for j in range(2, math.ceil(math.sqrt(i))):
         if i%j == 0:
             flag = 1
     if not flag:
@@ -16,3 +16,6 @@ for i in range(n):
 
 
 print(l)
+
+#pythonic way using list comprehension
+print([i for i in range(2,n) if len([1 for j in range(2,int(math.sqrt(i))+1) if i%j==0])==0])
